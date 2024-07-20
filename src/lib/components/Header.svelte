ebe34bc1b46fd848b1e61f534852e16c36dcb148
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-20 14:13:45 +0530

    basic structure

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,13 @@
+<script></script>
+<style>@import '../../styles/header.scss';</style>
+
+<header>
+  <div class="header--container">
+    <nav>
+      <div class="logo">
+        <img src="/logo.svg" alt="Logo" />
+      </div>
+      <div class="buttons"></div>
+    </nav>
+  </div>
+</header>
