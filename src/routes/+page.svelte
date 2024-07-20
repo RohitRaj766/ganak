98ee450b2e09fe6adc63a0f4789b448e3f4f17fa
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-20 11:06:41 +0530

    Initial Push

---
+++ src/routes/+page.svelte
@@ -0,0 +1,6 @@
+<style lang="scss">
+    @import '../styles/global.scss';
+
+</style>
+<h1 class="heading">Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
