2d2984f01926f4b1add5bfb8049d025c87f2bc2f
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-20 17:04:56 +0530

    navbar

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,13 +1,15 @@
 <script></script>
-<style>@import '../../styles/header.scss';</style>
+<style>@import '../../styles/header.css';</style>
 
 <header>
-  <div class="header--container">
-    <nav>
-      <div class="logo">
+    <nav class="header--container">
+      <div class="logo--container">
         <img src="/logo.svg" alt="Logo" />
+        <p class="title"> GANAK</p>
       </div>
-      <div class="buttons"></div>
+      <div class="buttons--container">
+        <button class="contact--us">Contact us</button>
+        <button class="book--demo">Book a demo</button>
+      </div>
     </nav>
-  </div>
 </header>
