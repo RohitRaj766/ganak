0ca1520a045406c9e4071794db6cf42b4eee500f
mohitrajjha111 <mohitrajjha111@gmail.com>
2024-07-22 17:40:01 +0530

    section-innovation added

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
-    import Header from '$lib/components/Header.svelte';
-    import Hero from '$lib/components/Hero.svelte'
-    import Limitation from '$lib/components/Limitation.svelte';
+  import Header from '$lib/components/Header.svelte';
+  import Hero from '$lib/components/Hero.svelte'
+	import Innovation from '$lib/components/Innovation.svelte';
+  import Limitation from '$lib/components/Limitation.svelte';
 </script>
 <style lang="scss">
   @import'../styles/global.scss';
@@ -13,6 +14,9 @@
   </div>
   <div class="second--child--layout">
     <Limitation/>
+  </div>
+  <div>
+    <Innovation/>
   </div>
 </div>
 
