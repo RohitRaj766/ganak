b804394a01bcbe20d6600e52433f0bb6bff284ff
mohitrajjha111 <mohitrajjha111@gmail.com>
2024-07-22 22:28:58 +0530

    section-innovation responsive

--- src/lib/components/Innovation.svelte
+++ src/lib/components/Innovation.svelte
@@ -2,6 +2,14 @@
     @import'../../styles/innovation.scss';
 </style>
 
-<section class="control">
-    <img src="/section-innovation.svg" alt="Innovation-info">
+<section >
+    <img class="big-screen control" src="/section-innovation.svg" alt="Innovation-info">
+    <img class="big-screen-1280 control" src="/section-innovation-1280.jpg" alt="Innovation-info">
+    <img class="big-screen-1200 control" src="/section-innovation-1200.jpg" alt="Innovation-info">
+    <img class="small-screen control" src="/section-innovation-mobile.svg" alt="Innovation-info">
+
+    <img class="big-screen-800 control" src="/section-innovation-800.png" alt="Innovation-info">
+    <img class="big-screen-720 control" src="/section-innovation-720.png" alt="Innovation-info">
+    <img class="big-screen-650 control" src="/section-innovation-650.png" alt="Innovation-info">
+
 </section>
