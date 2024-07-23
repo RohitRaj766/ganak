8ef35bb1eab0b23599d1abf3130e0c060aa4ff8e
mohitrajjha111 <mohitrajjha111@gmail.com>
2024-07-23 14:43:27 +0530

    footer added

---
+++ src/lib/components/Footer.svelte
@@ -0,0 +1,33 @@
+<style>
+    @import'../../styles/footer.scss';
+</style>
+
+<section class="footer">
+    <div>
+        <div class="container-footer">
+            <div class="logo-footer"><img src="/ganak-footer.svg" alt="Ganak Logo"></div>
+            <div class="text-footer">Connect with us on </div>
+            <div class="small-text small-footer"><span><i class="fa-brands fa-linkedin"></i> LinkedIn </span><span class="space"><i class="fa-brands fa-x-twitter"></i> Twitter </span></div>
+        </div>
+        <div class="container-footer">
+            <div class="contact-us">
+                <p class="normal-text">Contact Us</p>
+                <p class="small-text">Send us your message and we will get back to you!</p>
+                <div class="contact-form">
+                <form action="#" method="post">
+                    <input type="email" placeholder="Enter Your Email" required><br>
+                    <input type="text" placeholder="Message" required><br>
+                    <button type="submit">Send</button>
+                </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div>
+        <div class="footer-hidden">
+            <hr>
+            <div class="small-text small-footer show"><span><i class="fa-brands fa-linkedin"></i> LinkedIn </span><span class="space"><i class="fa-brands fa-x-twitter"></i> Twitter </span></div>
+        </div>
+        <p class="footer-note">Copyright <i class="fa-regular fa-copyright"></i> 2024.GANAK.All Rights Reserved.</p>
+    </div>
+</section>
