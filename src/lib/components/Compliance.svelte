2eb77d0f39319e9747e4a0274c546935be9f26f6
mohitrajjha111 <mohitrajjha111@gmail.com>
2024-07-26 21:30:48 +0530

    compliance-section-2 added

--- src/lib/components/Compliance.svelte
+++ src/lib/components/Compliance.svelte
@@ -15,13 +15,24 @@
         <img src="compliance.svg" alt="compliance-pic">
     </div>
     <div class="features-container">
-        <Features pic="/verification-delays.svg" title="Verification Delays" />
+        <!-- <Features pic="/verification-delays.svg" title="Verification Delays" />
         <Features pic="/complex-reporting.svg" title="Complex Reporting" />
         <Features pic="/data-overload.svg" title="Data Overload" />
         <Features pic="/high-operational-costs.svg" title="High Operational Costs" />
         <Features pic="/limited-transparency.svg" title="Limited Transparency" />
         <Features pic="/lag-in-response.svg" title="Lag in Response" />
         <Features pic="/enforcement-challenges.svg" title="Enforcement Challenges" />
-        <Features pic="/risk-exposure.svg" title="Risk Exposure" />
+        <Features pic="/risk-exposure.svg" title="Risk Exposure" /> -->
+
+        <Features pic="/instant-verification.svg" title="Instant Verification" /> 
+        <Features pic="/streamlined-reporting.svg" title="Streamlined Reporting" /> 
+        <Features pic="/structured-data-flow.svg" title="Structured Data Flow" /> 
+        <Features pic="/cost-efficiency.svg" title="Cost Efficiency" /> 
+        <Features pic="/transparent-monitoring.svg" title="Transparent Monitoring" /> 
+        <Features pic="/real-time-feeback.svg" title="Real-time Feedback" /> 
+        <Features pic="/swift-enforcement.svg" title="Swift Enforcement" /> 
+        <Features pic="/enhanced-feedback.svg" title="Enhanced Feedback" /> 
+
+        
     </div>
 </div>
