1302d36fadba7564955776a2d720455caecd1f22
mohitrajjha111 <mohitrajjha111@gmail.com>
2024-07-27 04:20:46 +0530

    innovation section made responsive

--- src/lib/components/Innovation.svelte
+++ src/lib/components/Innovation.svelte
@@ -3,13 +3,8 @@
 </style>
 
 <section >
+    <div class="image-responsive">
     <img class="big-screen control" src="/section-innovation.svg" alt="Innovation-info">
-    <img class="big-screen-1280 control" src="/section-innovation-1280.jpg" alt="Innovation-info">
-    <img class="big-screen-1200 control" src="/section-innovation-1200.jpg" alt="Innovation-info">
     <img class="small-screen control" src="/section-innovation-mobile.svg" alt="Innovation-info">
-
-    <img class="big-screen-800 control" src="/section-innovation-800.png" alt="Innovation-info">
-    <img class="big-screen-720 control" src="/section-innovation-720.png" alt="Innovation-info">
-    <img class="big-screen-650 control" src="/section-innovation-650.png" alt="Innovation-info">
-
+    </div>
 </section>
