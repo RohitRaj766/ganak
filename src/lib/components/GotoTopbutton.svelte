248ee69feda4c9143f81d650d5419b2c7db084f5
RohitRaj766 <rohitraj2k04@gmail.com>
2024-07-26 20:37:11 +0530

    Go to top button added

---
+++ src/lib/components/GotoTopbutton.svelte
@@ -0,0 +1,30 @@
+
+<script>
+    if (typeof window !== 'undefined') {
+        const handleScroll = () => {
+            const button = document.querySelector('.goto--top--button');
+            if (button) {
+                if (window.scrollY > 700) {
+                    button.style.display = 'flex';
+                } else {
+                    button.style.display = 'none';
+                }
+            }
+        };
+        window.addEventListener('scroll', handleScroll);
+            const button = document.querySelector('.goto--top--button');
+            if (button) {
+                button.addEventListener('click', () => {
+                    console.log("Button clicked");
+                    window.scrollTo({ top: 0, behavior: 'smooth' });
+                });
+                handleScroll();
+            }
+    }
+</script>
+<style>
+    @import'../../styles/goto-top.scss';
+</style>
+<div class="goto--top--button">
+    <i class="arrow-up"></i>
+</div>
